324d06495c0d8c93623d70ea0a1e391f8d38d585
muazatKabara <57442243+Muazat@users.noreply.github.com>
2023-08-27 21:04:29 +0100

    worked on interface and functionalities

---
+++ src/components/form.js
@@ -0,0 +1,15 @@
+
+const Form = () =>{
+    return(
+        <form>
+            <label>Restaurant</label><br></br>
+            <p>
+                <input type="text" placeholder="Enter your restaurant"/>
+            </p>
+            
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default Form
